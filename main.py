c3c1e8bac08763d94fcfd15aa146c722c17da95a
alamhasabie135 <alamhasabie165@gmail.com>
2020-07-07 21:27:31 +0700

    fix: added second file

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from modules import loader
 
 
 
-files = ['data/nucleotide/MT019529.1.fasta']
+files = ['data/nucleotide/MT019529.1.fasta', 'data/nucleotide/MT450872.1.fasta']
 loader = loader.Loader()
 sequence = loader.load_sequences(files)
+
